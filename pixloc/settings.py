b0c14c79bf522bc0c4d668c3529e4eb8a2d9902b
J Kabalar <32665838+jkabalar@users.noreply.github.com>
2021-12-12 18:37:31 +0100

    Update settings.py

--- pixloc/settings.py
+++ pixloc/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 root = Path(__file__).parent.parent  # top-level directory
-DATA_PATH = '../../kapture-localization/pipeline/examples' / 'datasets/'  # datasets and pretrained weights
+DATA_PATH = '../../kapture-localization/pipeline/examples/datasets/'  # datasets and pretrained weights
 TRAINING_PATH = root / 'outputs/training/'  # training checkpoints
 LOC_PATH = root / 'outputs/hloc/'  # localization logs
 EVAL_PATH = root / 'outputs/results/'  # evaluation results
